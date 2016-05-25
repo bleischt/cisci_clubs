efe1332175ec61c702f7b2557d96eb8ea5fb3d6e
Wasae Qureshi <wasaequreshi@gmail.com>
2016-05-25 10:38:29 -0700

    Added script to get files from frank

---
+++ get_frank_files.sh
@@ -0,0 +1,2 @@
+scp -r -i ~/.ssh/466key.pem qureshi466@frank.ored.calpoly.edu:/mnt/cisci/modules/clubs_resources/ .
+scp -r -i ~/.ssh/466key.pem qureshi466@frank.ored.calpoly.edu:/mnt/cisci/modules/clubs.py .
