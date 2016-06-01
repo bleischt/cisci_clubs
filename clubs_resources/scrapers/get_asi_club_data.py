36fd02e81701630676277a6cd65e02ef546ffe8b
Logan Williams <loganwilliams116@gmail.com>
2016-05-31 22:37:46 -0700

    Update asi scraper to filter only wanted clubs based on the id_to_club.json file. Also formatted the output in the dictionary format.

--- clubs_resources/scrapers/get_asi_club_data.py
+++ clubs_resources/scrapers/get_asi_club_data.py
@@ -2,7 +2,7 @@
 # (at http://www.asi.calpoly.edu/university_union/club_services).
 
 import urllib.request as request
-import bs4
+import bs4, json
 
 #asi_clubs_url = "http://www.asi.calpoly.edu/university_union/club_services"
 # It's actually here:
@@ -17,22 +17,45 @@ def get_asi_club_data():
         return get_club_data_from_soup(soup)
 
 def get_club_data_from_soup(soup):
-    data = []
+    data = {}
+
     for club_record in soup.find_all("li", "club_list"):
-        data.append(get_club_record_data(club_record))
+        title_data = get_club_record_data(club_record)
+        if is_related_club(title_data):
+            data[title_data[0]] = title_data[1]
+    
     return data
+
+def is_related_club(title_data):
+    """ Returns true if the given (title, data) tuple is a club listed in id_to_clubs.json """
+ 
+    title = title_data[0]
+    data = title_data[1]
+
+    clubs = None
+    with open('../id_to_club.json') as clubs_file:
+        clubs = json.load(clubs_file)
+
+    if title in clubs.keys():
+        return True
+
+    for club_name in clubs.keys():
+        if club_name.lower() in str(data.values()).lower():
+            return True
+
+    return False
 
 def get_club_record_data(club_record):
     club_data = {}
 
-    club_data["title"] = get_club_title(club_record)
+    club_title = get_club_title(club_record)
 
     club_detail_pairs = generate_club_detail_pairs(club_record)
 
     for (detail_title, detail) in club_detail_pairs:
         club_data[detail_title] = detail
 
-    return club_data
+    return (club_title, club_data)
     
 def get_club_title(club_record):
     return next(club_record.find("a", alt="Club Details").stripped_strings)
