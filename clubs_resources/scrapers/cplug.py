26c111af09fd4e2c3092b95be8c3841637672ae2
Logan Williams <loganwilliams116@gmail.com>
2016-06-09 15:40:07 -0700

    added officers scraper for cplug

--- clubs_resources/scrapers/cplug.py
+++ clubs_resources/scrapers/cplug.py
@@ -3,7 +3,9 @@
 from bs4 import BeautifulSoup
 import re, json, requests
 
-url = "http://cplug.org/about/a-brief-history-of-cplug/"
+
+history_url = "http://cplug.org/about/a-brief-history-of-cplug/"
+contact_url = "http://cplug.org/contact/"
 years_pos_people = {} 
 years = {}
 data_output_file = "../data/" + __file__.replace(".py", ".json")
@@ -23,8 +25,8 @@ def yr_range_to_list(yr_range):
 
     return yr_list
 
-def main():
-    myRequest = requests.get(url)
+def get_history():
+    myRequest = requests.get(history_url)
     soup = BeautifulSoup(myRequest.text, "html.parser")
     cur_p = soup.find('p')
      
@@ -53,9 +55,34 @@ def main():
               years_pos_people[yr] = pos_person_dict
         
        cur_p = cur_p.find_next('p')
+       return years_pos_people
 
+
+def get_current_officers():
+    myRequest = requests.get(contact_url)
+    soup = BeautifulSoup(myRequest.text, "html.parser")
+    cur_p = soup.find('section', {'class': 'post-content'})
+    cur_p = cur_p.find('ul', {'id': 'officers'})
+    
+    officers = {}
+    cur_p = cur_p.find('li')
+    list_p = cur_p
+    while list_p:
+       pos = list_p.find('h4').contents[0]
+       description = list_p.find('img').find('p').contents[0]
+       name = description.split()[0]
+       officers[name] = pos
+
+       cur_p = cur_p.find_next('li')
+       list_p = cur_p
+
+    return officers
+    
+   
+def main():
     main_cplug = {}
-    main_cplug['officers'] = years_pos_people
+    main_cplug['officers_history'] = get_history()
+    main_cplug['officers'] = get_current_officers()
 
     outfile = open(data_output_file, 'w')
     json.JSONEncoder().encode(main_cplug)
