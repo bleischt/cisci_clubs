8b72a030ed1848049dc8f67465a7d56b3723e03f
Michael Casebolt <mikebolt@gmail.com>
2016-06-08 21:15:47 -0700

    Add the two_level_distance function to sentence_distance.py

--- clubs_resources/sentence_distance.py
+++ clubs_resources/sentence_distance.py
@@ -12,7 +12,7 @@ def sentence_distance(sentence1, sentence2):
     #print("tokens1:", tokens1)
     #print("tokens2:", tokens2)
 
-    return levenshtein_distance(tokens1, tokens2, lambda x, y: x == y)
+    return two_level_distance(tokens1, tokens2)
 
 def characterwise_sentence_distance(sentence1, sentence2):
     return levenshtein_distance(sentence1, sentence2, lambda x, y: x == y)
@@ -21,10 +21,19 @@ def characterwise_sentence_distance(sentence1, sentence2):
 def compromise_sentence_distance(sentence1, sentence2):
     return sentence_distance(sentence1, sentence2) + 0.25 * characterwise_sentence_distance(sentence1, sentence2)
 
-def levenshtein_distance(sequence1, sequence2, equals):
-    return _levenshtein_distance_(sequence1, sequence2, equals, {})
+def unit_cost(n):
+    return lambda x, y: n
+
+difference_cost = lambda x, y: 0 if x == y else 1
+
+# Accepts 3 cost functions:
+# [0] calculates the cost if the first argument is removed
+# [1] calculates the cost if the second argument is removed
+# [2] calculates the cost if the arguments are both kept
+def levenshtein_distance(sequence1, sequence2, cost_functions=[unit_cost(1), unit_cost(1), difference_cost]):
+    return _levenshtein_distance_(sequence1, sequence2, cost_functions, {})
     
-def _levenshtein_distance_(sequence1, sequence2, equals, memory):
+def _levenshtein_distance_(sequence1, sequence2, cost_functions, memory):
     # Implementation inspired by the pseudocode on Wikipedia
     # https://wikipedia.org/wiki/Levenshtein_distance
     if len(sequence1) == 0:
@@ -39,15 +48,22 @@ def _levenshtein_distance_(sequence1, sequence2, equals, memory):
     if instance_key in memory:
         return memory[instance_key]
     else:
-        last_items_match = equals(sequence1[-1], sequence2[-1])
+        last1 = sequence1[-1]
+        last2 = sequence2[-1]
         
-        distance_if_1_shortened = _levenshtein_distance_(sequence1[:-1], sequence2, equals, memory) + 1
-        distance_if_2_shortened = _levenshtein_distance_(sequence1, sequence2[:-1], equals, memory) + 1
-        distance_if_characters_preserved = _levenshtein_distance_(sequence1[:-1], sequence2[:-1], equals, memory) + (0 if last_items_match else 1)
+        distance_if_1_shortened = _levenshtein_distance_(sequence1[:-1], sequence2, cost_functions, memory) + cost_functions[0](last1, last2)
+        distance_if_2_shortened = _levenshtein_distance_(sequence1, sequence2[:-1], cost_functions, memory) + cost_functions[1](last1, last2)
+        distance_if_characters_preserved = _levenshtein_distance_(sequence1[:-1], sequence2[:-1], cost_functions, memory) + cost_functions[2](last1, last2)
 
         result = min(distance_if_1_shortened, distance_if_2_shortened, distance_if_characters_preserved)
         memory[instance_key] = result
 
-        # print(instance_key + " : " + str(result))
-
         return result
+
+# Expects two lists of words.
+def two_level_distance(sequence1, sequence2):
+    first_removal_distance = lambda x, y: len(y)
+    second_removal_distance = lambda x, y: len(x)
+    comparison_distance = lambda x, y: levenshtein_distance(x, y)
+    cost_functions = [first_removal_distance, second_removal_distance, comparison_distance]
+    return levenshtein_distance(sequence1, sequence2, cost_functions)
